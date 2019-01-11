e1abef3948cb12a23771623225a61e42ab6f0831
deoladed <leo.robert.mk@gmail.com>
2019-01-11 10:23:10 +0100

    journalist ok

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -3,13 +3,7 @@ arr = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gael
 
 
 
-chiffres = 0
-arr.each do |i|
- if i.count("0-9") > 0
-chiffres += 1
-end
-end
-puts "Il y a #{chiffres} adresses contenant des chiffres"
+
 
 # Regex Style
 # count2 = 0
@@ -21,11 +15,7 @@ puts "Il y a #{chiffres} adresses contenant des chiffres"
 # puts count2
 
 
-aude = 0
-arr.each do |i|
-	aude +=1 if i.downcase.include?("aude")
-end
-puts "Il y a #{aude} adresses contenant le terme Aude"
+
 
 # maj = 0
 # arr.each do |i|
@@ -35,12 +25,7 @@ puts "Il y a #{aude} adresses contenant le terme Aude"
 
 
 
-maj = 0
-arr.each do |i|
-	maj +=1 if	i[1] == i[1].capitalize
-end
-puts "Il y a #{maj} adresses commancant par une majuscule"
-p "Il y a #{maj} adresses commancant par une majuscule"
+
 
 # puts "@BlutDDDDcdasa"[1] == /[[:upper:]]/
 # puts "A" == "a".capitalize  
@@ -66,20 +51,10 @@ i = i.scan /\w/
 end
 
 
-maj2 = 0
-arr.each do |i|
-	if capit(i) == true
-		maj2 += 1
-	end
-end
-puts "Il y a #{maj2} adresses contenant au moins une majuscule"
 
 
-underscr = 0
-arr.each do |i|
-    underscr += i.count("_")
-end
-puts "Il y a #{underscr} under_score dans les adresses"
+
+
 
 #Sort mais met tout en minuscules
 # arr2 = []
@@ -87,14 +62,7 @@ puts "Il y a #{underscr} under_score dans les adresses"
 # 	arr2 << i.downcase
 # end
 
-puts "Trions cette liste par ordre alphabetique :"
-puts arr.sort {|a, b| a.downcase <=> b.downcase }
-puts "--------------------------------------"
-puts "Voila les 50 adresses les plus courtes :"
-puts arr.sort{|a, b| a.size <=> b.size}.first(50)
 
-
-puts "Position de @epenser : #{arr.index {|mot| mot == "@epenser"} + 1}"
 
 
 
@@ -102,7 +70,7 @@ puts "Bonjour, que veux-tu savoir ?"
 
 puts "1 - Combien y a-t-il de journalistes dans ce array ?"
 puts "2 - Combien d'handle contiennent un numéro ?"
-puts "3 - Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?"
+puts "3 - Combien d'handle contiennent les 4 lettres du prénom 'Aude' à la suite (sans prendre en compte les majuscules) ?"
 puts "4 - Combien commencent par une majuscule (première lettre juste après le @) ?"
 puts "5 - Combien contiennent une majuscule ?"
 puts "6 - Combien y a-t-il de underscore _ dans tous les pseudos confondus ?"
@@ -110,7 +78,52 @@ puts "7 - Trie la liste de handle par ordre alphabétique."
 puts "8 - Quels sont les 50 handles les plus courts de ce array ?"
 puts "9 - Quelle est la position dans l'array de la personne @epenser ?"
 
-rep = gets.chomp
+rep = gets.chomp.to_i
 case rep
 	when 1 
 		puts "Il y a #{arr.length} journalistes dans ce fichier"
+	when 2
+		chiffres = 0
+		arr.each do |i|
+			if i.count("0-9") > 0
+				chiffres += 1
+			end
+		end
+		puts "Il y a #{chiffres} adresses contenant des chiffres"
+	when 3
+		aude = 0
+		arr.each do |i|
+			aude +=1 if i.downcase.include?("aude")
+		end
+		puts "Il y a #{aude} adresses contenant le terme Aude"
+	when 4
+		maj = 0
+		arr.each do |i|
+			maj +=1 if	i[1] == i[1].capitalize
+		end
+		puts "Il y a #{maj} adresses commancant par une majuscule"
+	when 5
+		maj2 = 0
+		arr.each do |i|
+			if capit(i) == true
+				aj2 += 1
+			end
+		end
+		puts "Il y a #{maj2} adresses contenant au moins une majuscule"
+	when 6
+		underscr = 0
+		arr.each do |i|
+    		underscr += i.count("_")
+		end
+		puts "Il y a #{underscr} under_score dans les adresses"
+	when 7
+		puts "Trions cette liste par ordre alphabetique :"
+		puts arr.sort {|a, b| a.downcase <=> b.downcase }
+	when 8
+		puts "Voila les 50 adresses les plus courtes :"
+		puts arr.sort{|a, b| a.size <=> b.size}.first(50)
+	when 9
+		puts "Position de @epenser : #{arr.index {|mot| mot == "@epenser"} + 1}"
+	else 
+		puts "Entre un nombre valide stp"
+	end
